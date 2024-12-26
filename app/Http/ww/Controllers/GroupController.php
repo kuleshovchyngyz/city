aaa176c8eb3e4cf4ad31ac56a7e758521ba25e59
maksat607 <kuleshov.chyngyz@gmail.com>
2024-12-26 18:40:55 +0600

    adding order

--- app/Http/ww/Controllers/GroupController.php
+++ app/Http/ww/Controllers/GroupController.php
@@ -384,7 +384,7 @@ class GroupController extends Controller
 
 
 
-    public function searchAskar(Request $request)
+    public function searchAskar1(Request $request)
     {
         return AskarCityResource::collection(
             geo_cities::when($request->get('city'), function ($q) use ($request) {
@@ -418,6 +418,80 @@ class GroupController extends Controller
                 ->get()
         );
     }
+
+    public function searchAskar(Request $request)
+    {
+        // Log the incoming request for debugging
+        Log::info('Search Request:', $request->all());
+
+        // Fetch and sort cities
+        $query = geo_cities::when($request->get('city'), function ($q) use ($request) {
+            $q->where('name', 'like', '%' . $request->city . '%');
+        })
+            ->with(['region', 'district'])
+            ->when($request->has('city_id'), function ($query) {
+                $query->where('id', request('city_id'));
+            })
+            ->when($request->has('region'), function ($query) {
+                $query->whereHas('region', function ($query) {
+                    $query->where('name', 'like', '%' . request('region') . '%');
+                });
+            })
+            ->when($request->has('district'), function ($query) {
+                $query->whereHas('district', function ($query) {
+                    $query->where('name', 'like', '%' . request('district') . '%');
+                });
+            })
+            ->when($request->has('region_id'), function ($query) {
+                $query->whereHas('region', function ($query) {
+                    $query->where('id', request('region_id'));
+                });
+            })
+            ->when($request->has('district_id'), function ($query) {
+                $query->whereHas('district', function ($query) {
+                    $query->where('id', request('district_id'));
+                });
+            })
+            ->orderBy('order', 'asc') // Sort by `order` column
+            ->toSql();
+
+        // Log the query to verify `orderBy` is applied
+        Log::info('Generated Query:', ['query' => $query]);
+
+        // Execute and return results
+        $results = geo_cities::when($request->get('city'), function ($q) use ($request) {
+            $q->where('name', 'like', '%' . $request->city . '%');
+        })
+            ->with(['region', 'district'])
+            ->when($request->has('city_id'), function ($query) {
+                $query->where('id', request('city_id'));
+            })
+            ->when($request->has('region'), function ($query) {
+                $query->whereHas('region', function ($query) {
+                    $query->where('name', 'like', '%' . request('region') . '%');
+                });
+            })
+            ->when($request->has('district'), function ($query) {
+                $query->whereHas('district', function ($query) {
+                    $query->where('name', 'like', '%' . request('district') . '%');
+                });
+            })
+            ->when($request->has('region_id'), function ($query) {
+                $query->whereHas('region', function ($query) {
+                    $query->where('id', request('region_id'));
+                });
+            })
+            ->when($request->has('district_id'), function ($query) {
+                $query->whereHas('district', function ($query) {
+                    $query->where('id', request('district_id'));
+                });
+            })
+            ->orderBy('order', 'asc')
+            ->get();
+
+        return AskarCityResource::collection($results);
+    }
+
 
 
     public function showCity(Request $request, geo_cities $city)
