875d569c3926af73f58d7f4986ba8b0255f031b2
maksat607 <kuleshov.chyngyz@gmail.com>
2024-12-26 18:52:26 +0600

    adding new city controller

--- app/Http/Controllers/NewCityController.php
+++ app/Http/Controllers/NewCityController.php
@@ -14,40 +14,6 @@ class NewCityController extends Controller
         // Log the incoming request for debugging
         Log::info('Search Request:', $request->all());
 
-        // Fetch and sort cities
-        return $query = geo_cities::when($request->get('city'), function ($q) use ($request) {
-            $q->where('name', 'like', '%' . $request->city . '%');
-        })
-            ->with(['region', 'district'])
-            ->when($request->has('city_id'), function ($query) {
-                $query->where('id', request('city_id'));
-            })
-            ->when($request->has('region'), function ($query) {
-                $query->whereHas('region', function ($query) {
-                    $query->where('name', 'like', '%' . request('region') . '%');
-                });
-            })
-            ->when($request->has('district'), function ($query) {
-                $query->whereHas('district', function ($query) {
-                    $query->where('name', 'like', '%' . request('district') . '%');
-                });
-            })
-            ->when($request->has('region_id'), function ($query) {
-                $query->whereHas('region', function ($query) {
-                    $query->where('id', request('region_id'));
-                });
-            })
-            ->when($request->has('district_id'), function ($query) {
-                $query->whereHas('district', function ($query) {
-                    $query->where('id', request('district_id'));
-                });
-            })
-            ->orderBy('order', 'asc') // Sort by `order` column
-            ->toSql();
-
-        // Log the query to verify `orderBy` is applied
-        Log::info('Generated Query:', ['query' => $query]);
-
         // Execute and return results
         $results = geo_cities::when($request->get('city'), function ($q) use ($request) {
             $q->where('name', 'like', '%' . $request->city . '%');
