acff6a2f9230dafc5bcfebb63ddbf482fec1ea8b
maksat607 <kuleshov.chyngyz@gmail.com>
2025-09-30 14:35:57 +0600

    cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -3,35 +3,15 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\Http\Request;
 
 class Cors
 {
     public function handle($request, Closure $next)
     {
-        $allowedOrigins = [
-            'http://localhost:5173',
-            // Add other allowed origins here
-        ];
-        
-        $origin = $request->header('Origin');
-        
-        // For preflight requests
-        if ($request->isMethod('OPTIONS')) {
-            $response = response('', 200);
-        } else {
-            $response = $next($request);
-        }
-
-        if (in_array($origin, $allowedOrigins)) {
-            return $response
-                ->header('Access-Control-Allow-Origin', $origin)
-                ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-                ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, X-CSRF-TOKEN, Access-Control-Allow-Origin')
-                ->header('Access-Control-Allow-Credentials', 'true')
-                ->header('Access-Control-Max-Age', '86400');
-        }
-        
-        return $next($request);
+        return $next($request)
+            ->header('Access-Control-Allow-Origin', 'https://test.vinz.ru')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Access-Control-Allow-Origin')
+            ->header('Access-Control-Allow-Credentials', 'true');
     }
 }
---
+++ app/Http/Middleware/CorsMiddleware.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class CorsMiddleware
+{
+    public function handle($request, Closure $next)
+    {
+        \Log::info('CORS Middleware hit', [
+            'origin' => $request->header('Origin'),
+            'method' => $request->method(),
+            'path' => $request->path()
+        ]);
+        $allowedOrigins = [
+            'https://test.vinz.ru',
+            'https://www.test.vinz.ru',
+            'http://localhost:5173'
+        ];
+
+        $origin = $request->header('Origin');
+        
+        if (in_array($origin, $allowedOrigins)) {
+            $headers = [
+                'Access-Control-Allow-Origin'      => $origin,
+                'Access-Control-Allow-Methods'     => 'GET, POST, PUT, DELETE, OPTIONS',
+                'Access-Control-Allow-Headers'     => 'Content-Type, Authorization, X-Requested-With, X-CSRF-TOKEN, Accept',
+                'Access-Control-Allow-Credentials' => 'true',
+                'Vary' => 'Origin'
+            ];
+
+            if ($request->isMethod('OPTIONS')) {
+                return response()->json('OK', 200, $headers);
+            }
+
+            $response = $next($request);
+            
+            foreach($headers as $key => $value) {
+                $response->headers->set($key, $value);
+            }
+            
+            return $response;
+        }
+
+        return $next($request);
+    }
+}
